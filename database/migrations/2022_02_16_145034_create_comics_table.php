b4cbfe12f0e4263665baf6612f50101a7ad6aa6a
salzanopm <salzanopm@gmail.com>
2022-02-16 17:18:11 +0100

    seeder created

---
+++ database/migrations/2022_02_16_145034_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 50);
+            $table->mediumText('description');
+            $table->string('thumb', 150);
+            $table->decimal('price', 8, 2);
+            $table->string('series', 40);
+            $table->date('sale_date');
+            $table->string('type', 20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
